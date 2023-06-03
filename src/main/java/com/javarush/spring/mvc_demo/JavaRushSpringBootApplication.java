3bc1ee75372dffb20f810e9a8c11cb8ac26e9989
artem <iakonien@gmail.com>
2023-06-03 22:58:48 +0300

    HelloSpringMVC

---
+++ src/main/java/com/javarush/spring/mvc_demo/JavaRushSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javarush.spring.mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRushSpringBootApplication.class, args);
+    }
+
+}
